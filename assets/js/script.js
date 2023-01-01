3c36bc453da607eb07f0b31b3465201b5f081df6
Dhanushss017 <117816081+Dhanushss017@users.noreply.github.com>
2023-01-01 12:36:17 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,46 @@
-'use strict';
+// Car colours
 
+jQuery(document).ready(function ($) {
+  $(".bg_copper").on({
+      click: function () {
+          $("#change-image").attr(
+              "src",
+              "https://media.istockphoto.com/id/1338726735/photo/abstract-cityscape.jpg?b=1&s=170667a&w=0&k=20&c=y69niDRjFSrtSdchRIAkhtEMAwWV0kKGuaFbvYRpwVE="
+          );
+      },
+  });
 
+  $(".bg_fiery-red").on({
+      click: function () {
+          $("#change-image").attr(
+              "src",
+              "https://media.istockphoto.com/id/1400925699/photo/senju-fall.jpg?b=1&s=170667a&w=0&k=20&c=j3i3iLMezlwIWiB7K4ztyIZV66NMlp6w0AbUgQR7X9c="
+          );
+      },
+  });
 
-/**
- * navbar toggle
- */
+  $(".bg_fiery-red-black").on({
+      click: function () {
+          $("#change-image").attr(
+              "src",
+              "https://media.istockphoto.com/id/1248619427/photo/retro-futuristic-city-flythrough-background-80s-sci-fi-landscape-in-space.jpg?b=1&s=170667a&w=0&k=20&c=hLmRgItKjP0h5IboMbZC-TSp63ThuncI9880Glb1m_Y="
+          );
+      },
+  });
 
-const header = document.querySelector("[data-header]");
-const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
-
-navToggleBtn.addEventListener("click", function () {
-  header.classList.toggle("nav-active");
-  this.classList.toggle("active");
+  $(".bg_white").on({
+      click: function () {
+          $("#change-image").attr(
+              "src",
+              "https://media.istockphoto.com/id/1363681781/photo/blue-night-starry-sky-with-glowing-stars-bright-glow-of-sky-stars-and-milky-way-galaxy-4k.jpg?b=1&s=170667a&w=0&k=20&c=ZJsVEXvrkyadmgNmheR_7XVXp2btNB35pF0p93BJlQM="
+          );
+      },
+  });
 });
 
-/**
- * toggle the navbar when click any navbar link
- */
-
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
-
-for (let i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", function () {
-    header.classList.toggle("nav-active");
-    navToggleBtn.classList.toggle("active");
+$(document).ready(function () {
+  $(".btn-color").click(function (ev) {
+      $(".btn-color.active").removeClass("active");
+      $(this).addClass("active");
   });
-}
-
-
-
-
-
-/**
- * back to top & header
- */
-
-const backTopBtn = document.querySelector("[data-back-to-top]");
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
 });
